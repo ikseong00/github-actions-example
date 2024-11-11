1f68444896f1418a7e791b80a84bcd45b100933b
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2024-11-12 00:21:18 +0900

    gradle 버전, compile sdk 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.github_actions_example"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.github_actions_example"
